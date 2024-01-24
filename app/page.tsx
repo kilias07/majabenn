bc71ed0e6b417168a4f197ccc2dd642ed00ef575
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2024-01-24 15:43:57 +0100

    fix view on mobile vh into vhs2

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
     <main className="relative">
-      <div className="flex">
+      <div className="flex h-[100svh] w-full">
         <div
-          className="w-1/2 h-[100svh] bg-[#F5F0F6] flex justify-center flex-col md:pr-10 pr-4">
+          className="w-1/2 h-full bg-[#F5F0F6] flex justify-center flex-col md:pr-10 pr-4">
           <h1 className="text-3xl md:text-5xl font-display text-right">Maja Benn</h1>
           <p className="text-right text-base  md:text-lg mt-4">
             kontakt@majabenn.pl
           </p>
         </div>
-        <div className="w-1/2 h-screen bg-[#0C1B33] flex items-center">
+        <div className="w-1/2 h-full bg-[#0C1B33] flex items-center">
           <ul className="pl-4 md:pl-10 text-gray-400 text-sm md:text-lg flex flex-col gap-2 relative after:contet-[''] after:absolute after:w-[140%] after:h-1 after:-left-[70%] after:-bottom-4 after:bg-[#9D9171] after:rounded-2xl">
             <li>Konsultacje psychogeriatryczne</li>
             <li>Doradztwo psychologiczne</li>
